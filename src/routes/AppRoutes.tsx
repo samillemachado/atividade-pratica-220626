657da9896349f529a24d46a95e195b0420a8f611
Samille Machado <sami.bmachado@gmail.com>
2022-06-29 09:44:04 -0300

    react deploy no github

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,7 @@ import Home from '../pages/Home'
 
 const AppRoutes: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
